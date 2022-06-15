9451bf1d0132ca5363a16dc4f50e7ad09c45d4b3
mustafa <mustafayegnidemir1@gmail.com>
2022-06-15 16:21:47 +0300

    constructor configured

---
+++ src/app/model.ts
@@ -0,0 +1,24 @@
+export class Model {
+  user;
+  items;
+
+  constructor(){
+    this.user="Mustafa";
+    this.items= [
+                  new ToDoItem("Spor", false ),
+                  new ToDoItem("Kahvaltı", false ),
+                  new ToDoItem("Kitap Okumak", false ),
+                  new ToDoItem("Sinema", false )
+                ];
+  }
+}
+
+export class ToDoItem {
+  description: String;
+  action: boolean;
+
+  constructor(description: String, action : boolean){
+    this.description = description;
+    this.action = action;
+  }
+}
